85e26b206750b2c6d452f91b2f08cd9c272e05dd
Nour <nfa52@mail.aub.edu>
2025-04-13 21:01:11 +0300

    added props and responsivness

--- src/components/BranchCard.tsx
+++ src/components/BranchCard.tsx
@@ -1,33 +1,44 @@
 import React from "react";
 import styles from "./Card.module.css";
-import img from "../assets/branch.png";
 import { GrLocationPin } from "react-icons/gr";
 import { LuPhone } from "react-icons/lu";
 import { FiClock } from "react-icons/fi";
 import { IoMapOutline } from "react-icons/io5";
-const card = () => {
+
+type BranchProps = {
+  name: string;
+  address: string;
+  phone: string;
+  hours: string;
+  mapUrl: string;
+  image: string; // image URL or import
+};
+
+const BranchCard = ({ name, address, phone, hours, mapUrl, image }: BranchProps) => {
   return (
     <div className={styles.container}>
-      <img src={img.src} alt="..."></img>
+      <img src={image} alt={name} />
       <div className={styles.sub}>
-        <h2>Branch Name</h2>
+        <h2>{name}</h2>
 
         <div className={styles.info}>
           <div className={styles.other}>
             <GrLocationPin size={25} />
-            <p>Address</p>
+            <p>{address}</p>
           </div>
           <div className={styles.other}>
             <LuPhone size={25} />
-            <p>Phone Number</p>
+            <p>{phone}</p>
           </div>
           <div className={styles.other}>
             <FiClock size={25} />
-            <p>Opening Hours</p>
+            <p>{hours}</p>
           </div>
           <div className={styles.other}>
             <IoMapOutline size={25} />
-            <a>check location on google maps</a>
+            <a href={mapUrl} target="_blank" rel="noopener noreferrer">
+              Check location on Google Maps
+            </a>
           </div>
         </div>
       </div>
@@ -35,4 +46,4 @@ const card = () => {
   );
 };
 
-export default card;
+export default BranchCard;
