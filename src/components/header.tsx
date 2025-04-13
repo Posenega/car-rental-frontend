b6afa1df723cb20b4b270405c3a59a2117678a84
Nour <nfa52@mail.aub.edu>
2025-04-13 20:29:25 +0300

    added dropdown

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,41 +1,60 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import styles from "./header.module.css";
 import { MdPersonOutline } from "react-icons/md";
 import { HiOutlineShoppingCart } from "react-icons/hi";
-import { IoCarSportOutline } from "react-icons/io5"; // Using icons
-const header = () => {
+import { IoCarSportOutline } from "react-icons/io5";
+
+const Header = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
   return (
     <header>
       <div className={styles.page}>
         <main className={styles.main}>
-          {/* Navbar */}
           <header className={styles.navbar}>
             <nav className={styles.navContainer}>
               <a href="/" className={styles.icon}>
                 <IoCarSportOutline size={24} />
               </a>
+
               <div>
                 <a href="/">Home</a>
                 <a href="/cars">Cars</a>
                 <a href="/reservation">Reservation</a>
                 <a href="/reviews">Reviews</a>
               </div>
-              <div>
+
+              <div className={styles.rightIcons}>
                 <a href="/cart" className={styles.icon}>
                   <HiOutlineShoppingCart size={20} />
                 </a>
-                <a href="/profile" className={styles.icon}>
-                  <MdPersonOutline size={24} />
-                </a>
+                <div className={styles.dropdownWrapper}>
+                  <div
+                    className={styles.icon}
+                    onClick={() => setShowDropdown(!showDropdown)}
+                  >
+                    <MdPersonOutline size={24} />
+                  </div>
+                  {showDropdown && (
+                    <div className={styles.dropdown}>
+                      <a href="/login">Log in</a>
+                      <a href="/signup">Sign up</a>
+                      <a href="/profile">Profile</a>
+                      <hr />
+                      <a href="/points">🎖️ Points</a>
+                      <a href="/support">💬 Chat Support</a>
+                    </div>
+                  )}
+                </div>
               </div>
             </nav>
           </header>
-
-          {/* About Us Content */}
         </main>
       </div>
     </header>
   );
 };
 
-export default header;
+export default Header;
